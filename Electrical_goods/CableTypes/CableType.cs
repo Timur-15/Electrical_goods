238499b6aab3210430a211b2f44f182ed1fd12a5
stud <stud@07-415-01>
2025-06-10 11:25:03 +0300

    Добавьте файлы проекта.

---
+++ Electrical_goods/CableTypes/CableType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electrical_goods.CableTypes
+{
+    internal class CableType
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public CableType(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public string DisplayName => $"{Name} - {Price} за дюйм";
+    }
+}
---
+++ Electrical_goods/MainForm.cs
@@ -0,0 +1,15 @@
+namespace Electrical_goods
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
